d970125738567b54c5dd6b65b2a868ae13147d1c
Ellinnaa <elenawiedmann@googleail.com>
2025-03-20 18:05:54 +0100

    Karten code in main.js

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("Hallo!");
+var map = L.map('map').setView([-35.183333, 174.166667], 11);
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+        var marker = L.marker([-35.183333, 174.166667]).addTo(map);
+        marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
