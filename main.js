7689343663d83eede8d7f4c3961c7d4dbe876985
Ellinnaa <elenawiedmann@googleail.com>
2025-03-27 16:31:43 +0100

    stop-objekt in der Karte verwendet

--- main.js
+++ main.js
@@ -1,22 +1,17 @@
 /*Skript für Neuseelandreise*/
 
-let lat = -35.183333;
-let long = 174.166667;
-let zoom = 11;
-
 let stop= {
     nr: 2,
-    titel: "Bay of Islands",
+    title: "Bay of Islands",
     user: "Ellinnaa",
     lat:-35.183333,
     long:174.166667,
     zoom:11,
 };
 
-console.log
 
 //Karte initialisieren
-let map = L.map('map').setView([lat, long], zoom);
+let map = L.map('map').setView([stop.lat, stop.long], stop.zoom);
 //Hintergrundkarte initialisieren
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -24,12 +19,12 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 //Marker zeichnen
-let marker = L.marker([-35.183333, 174.166667]).addTo(map);
+let marker = L.marker([stop.lat, stop.long]).addTo(map);
 //Popup definieren und öffnen
 marker.bindPopup(`
-            <h2> Bay of Islands</h2>
+            <h2>${stop.title}</h2>
             <ul>
-            <li>Geografische Breite ${lat.toFixed(5)}° </li>
-            <li>Geografische Länge ${long.toFixed(5)}° </li>
+            <li>Geografische Breite ${stop.lat. toFixed(5)}° </li>
+            <li>Geografische Länge ${stop.long. toFixed(5)}° </li>
             </ul>
             `).openPopup();
