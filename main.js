8fb794089c7789fb327e819d39c1440f6b5f93d0
Ellinnaa <elenawiedmann@googleail.com>
2025-03-27 15:36:32 +0100

    Kommentare einfügen in main.js

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
+/*Skript für Neuseelandreise*/
+
+//Karte initialisieren
 var map = L.map('map').setView([-35.183333, 174.166667], 11);
+//Hintergrundkarte initialisieren
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);
+
+//Marker zeichnen
         var marker = L.marker([-35.183333, 174.166667]).addTo(map);
+        //Popup definieren und öffnen
         marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
